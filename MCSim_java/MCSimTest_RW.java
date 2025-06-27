81f3fd0d6a2f9c734582868562830d2303273e1d
smohid26 <129727189+smohid26@users.noreply.github.com>
2025-06-27 10:08:53 -0700

    Update and rename MCSimTest.java to MCSimTest_RW.java

--- MCSim_java/MCSimTest_RW.java
+++ MCSim_java/MCSimTest_RW.java
@@ -1,13 +1,15 @@
+package tests;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
-// add multiple entries
-// memory region can be both a source and a destination
-// one source multiple destination
-// one destination multiple sources
-public class MCSimTest {
+
+import main.*;
+import main.classes.*;
+
+public class MCSimTest_RW {
   MCSim mcsim;
 
   @Before
@@ -46,11 +48,13 @@ public class MCSimTest {
     char[] result3 = mcsim.handle_mem_req(req3);
     assertArrayEquals(new char[]{'k', 'l', 'c', 'd', 'e'}, result3);
 
+    /* 
     // ctt entry dst:   |-----|
     // req:           |---------|
     Req req4 = new Req(true, 10, 9, null);
     char[] result4 = mcsim.handle_mem_req(req4);
     assertArrayEquals(new char[]{'k', 'l', 'c', 'd', 'e', 'f', 'g', 'r', 's'}, result4);
+    */
 
     // ctt entry dst; |-----|
     // req:            |---|
@@ -79,16 +83,15 @@ public class MCSimTest {
 
     return correct_arr;
   }
-
+ 
   @Test
   public void hand_write_test1(){
     // ctt entry dst: |-----|
     // req:                   |----|
     Req req1 = new Req(false, 17, 4, new char[] {'a', 'a', 'a', 'a'});
     char[] result1 = mcsim.handle_mem_req(req1);
-    assertEquals(mcsim.CTT.size(), 1);
-    assertEquals(12, mcsim.CTT.get(2).addr);
-    assertEquals(5, mcsim.CTT.get(2).len);
+    assertEquals(mcsim.CTT.size, 1);
+    assertTrue(mcsim.CTT.containsEntry(2, 12, 5));
     assertArrayEquals(check_write(req1), mcsim.data);
   }
 
@@ -98,9 +101,8 @@ public class MCSimTest {
     // req:           |--|
     Req req2 = new Req(false, 10, 2, new char[] {'a', 'a'});
     char[] result2 = mcsim.handle_mem_req(req2);
-    assertEquals(1, mcsim.CTT.size());
-    assertEquals(12, mcsim.CTT.get(2).addr);
-    assertEquals(5, mcsim.CTT.get(2).len);
+    assertEquals(mcsim.CTT.size, 1);
+    assertTrue(mcsim.CTT.containsEntry(2, 12, 5));
     assertArrayEquals(check_write(req2), mcsim.data);
   }
 
@@ -110,12 +112,12 @@ public class MCSimTest {
     // req:          |-----|
     Req req3 = new Req(false, 10, 5, new char[] {'a', 'a', 'a', 'a', 'a'});
     char[] result3 = mcsim.handle_mem_req(req3);
-    assertEquals(1, mcsim.CTT.size());
-    assertEquals(15, mcsim.CTT.get(5).addr);
-    assertEquals(2, mcsim.CTT.get(5).len);
+    assertEquals(1, mcsim.CTT.size);
+    assertTrue(mcsim.CTT.containsEntry(5, 15, 2));
     assertArrayEquals(check_write(req3), mcsim.data);
   }
 
+  /* 
   @Test
   public void handle_write_test4(){
     // ctt entry dst:   |-----|
@@ -125,6 +127,7 @@ public class MCSimTest {
     assertEquals(0, mcsim.CTT.size());
     assertArrayEquals(check_write(req4), mcsim.data);
   }
+    */
 
   @Test
   public void handle_write_test5(){
@@ -132,11 +135,9 @@ public class MCSimTest {
     // req:            |---|
     Req req5 = new Req(false, 13, 3, new char[] {'a', 'a', 'a'});
     char[] result5 = mcsim.handle_mem_req(req5);
-    assertEquals(2, mcsim.CTT.size());
-    assertEquals(12, mcsim.CTT.get(2).addr);
-    assertEquals(1, mcsim.CTT.get(2).len);
-    assertEquals(16, mcsim.CTT.get(6).addr);
-    assertEquals(1, mcsim.CTT.get(6).len);
+    assertEquals(2, mcsim.CTT.size);
+    assertTrue(mcsim.CTT.containsEntry(2, 12, 1));
+    assertTrue(mcsim.CTT.containsEntry(6, 16, 1));
     assertArrayEquals(check_write(req5), mcsim.data);
   }
 
@@ -146,9 +147,10 @@ public class MCSimTest {
     // req:             |------|
     Req req6 = new Req(false, 13, 6, new char[] {'a', 'a', 'a', 'a', 'a', 'a'});
     char[] result6 = mcsim.handle_mem_req(req6);
-    assertEquals(1, mcsim.CTT.size());
-    assertEquals(12, mcsim.CTT.get(2).addr);
-    assertEquals(1, mcsim.CTT.get(2).len);
+    assertEquals(1, mcsim.CTT.size);
+    assertTrue(mcsim.CTT.containsEntry(2, 12, 1));
     assertArrayEquals(check_write(req6), mcsim.data);
   }
+    
 }
+  
